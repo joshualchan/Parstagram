76207b5313b6638c1828178950d9de08a17e8ba9
Joshua Chan <joshualchan@gmail.com>
2020-05-07 13:39:09 -0700

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Joshua Chan on 5/7/20.
+//  Copyright © 2020 Joshua Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
